65f26748a989786053919860fadc5f30532f9011
Camilo Romero <militoromero10@gmail.com>
2021-07-12 12:03:06 -0500

    initial project added

---
+++ src/main/java/com/milo/config/server/MiloConfigServerApplication.java
@@ -0,0 +1,13 @@
+package com.milo.config.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiloConfigServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MiloConfigServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/milo/config/server/MiloConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.milo.config.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MiloConfigServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
